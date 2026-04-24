16f7985f527a8aca945d87b0b55e2233391fcca0
Antonin Peronnet <62420525+rambip@users.noreply.github.com>
2026-04-24 20:26:40 +0200

    Fix download path for AppImageLauncher RPM

--- files/scripts/install_appimagelauncher.sh
+++ files/scripts/install_appimagelauncher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -Lo https://github.com/TheAssassin/AppImageLauncher/releases/download/v3.0.0-beta-3/appimagelauncher_3.0.0-beta-2-gha287.96cb937_x86_64.rpm
+curl -Lo /tmp/appimagelauncher.rpm https://github.com/TheAssassin/AppImageLauncher/releases/download/v3.0.0-beta-3/appimagelauncher_3.0.0-beta-2-gha287.96cb937_x86_64.rpm
 rpm -i --nodigest --nosignature --noscripts /tmp/appimagelauncher.rpm
